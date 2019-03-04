46341a922f604e00c3021eca9fcf027c133a2cc2
Matheus <matheus.brasil10@gmail.com>
2019-03-04 19:00:14 -0300

    Bootstrap project basic structure (#7)
    * chore(project): dotfiles
    
    * chore(vscode): base settings and recommended extensions
    
    * chore(now): initial deploy setup
    
    * chore(typescript): initial compiler and linter setup
    
    * chore(next): initial setup
    
    * chore(custom_typings): node
    
    * chore(package): dependencies and scripts
    
    * feat(src): bootstrap static content

---
+++ next.config.js
@@ -0,0 +1,55 @@
+const withPlugins = require('next-compose-plugins');
+
+const withTypescript = require('@zeit/next-typescript');
+const withProgressBar = require('next-progressbar');
+const withOffline = require('next-offline');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
+const withCSS = require('@zeit/next-css');
+
+const {resolve} = require('path');
+
+const {NODE_ENV = 'development'} = process.env;
+
+const nextConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
+  distDir: '../.next',
+  webpack: (config, options) => {
+    config.plugins = config.plugins || [];
+
+    if (!options.isServer) {
+      config.output.globalObject = 'self';
+    }
+
+    if (NODE_ENV === 'development' && !options.isServer) {
+      config.watchOptions = {
+        ignored: [/\.git[\\/]/, /\.next[\\/]/, /node_modules/],
+      };
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          ignoreDiagnostics: [6133, 6196],
+          tsconfig: resolve(__dirname, './tsconfig.json'),
+          tslint: resolve(__dirname, './tslint.json'),
+        }),
+      );
+    }
+
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [[withProgressBar], [withTypescript], [withBundleAnalyzer], [withOffline], [withCSS]],
+  nextConfig,
+);
