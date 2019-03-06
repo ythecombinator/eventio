f87822378d3e2f8fb2774bc8dbea5f13e073a853
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-06 07:01:45 -0300

    fix(components/Layout): remove next-page-transitions

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,11 +1,7 @@
 import React, {Fragment} from 'react';
-import {PageTransition} from 'next-page-transitions';
-
 import {loadFonts} from 'utils/fonts';
 
-import {GlobalStyles, PageTransitionView} from './style';
-
-export const transitionTimeout = 400;
+import {GlobalStyles} from './style';
 
 const Layout = (props) => {
   const {children} = props;
@@ -13,23 +9,10 @@ const Layout = (props) => {
   loadFonts();
 
   return (
-    <PageTransitionView>
-      <PageTransition
-        timeout={transitionTimeout}
-        classNames="page-transition"
-        loadingDelay={0}
-        loadingTimeout={{
-          enter: transitionTimeout,
-          exit: 0,
-        }}
-        loadingClassNames="loading-indicator"
-      >
-        <Fragment>
-          <GlobalStyles />
-          {children}
-        </Fragment>
-      </PageTransition>
-    </PageTransitionView>
+    <Fragment>
+      <GlobalStyles />
+      {children}
+    </Fragment>
   );
 };
 
--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -1,37 +1,5 @@
 import {createGlobalStyle} from 'styled-components';
 
-import styled from 'utils/theme';
-
-import {transitionTimeout} from './';
-
-const PageTransitionView = styled.div`
-  .page-transition-enter {
-    opacity: 0;
-    transform: translate3d(0, 20px, 0);
-  }
-  .page-transition-enter-active {
-    opacity: 1;
-    transform: translate3d(0, 0, 0);
-    transition: opacity ${transitionTimeout}ms, transform ${transitionTimeout}ms;
-  }
-  .page-transition-exit {
-    opacity: 1;
-  }
-  .page-transition-exit-active {
-    opacity: 0;
-    transition: opacity ${transitionTimeout}ms;
-  }
-  .loading-indicator-appear,
-  .loading-indicator-enter {
-    opacity: 0;
-  }
-  .loading-indicator-appear-active,
-  .loading-indicator-enter-active {
-    opacity: 1;
-    transition: opacity ${transitionTimeout}ms;
-  }
-`;
-
 const GlobalStyles = createGlobalStyle`
   * { box-sizing: border-box; }
   a { text-decoration: none; }
@@ -47,4 +15,4 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-export {PageTransitionView, GlobalStyles};
+export {GlobalStyles};
