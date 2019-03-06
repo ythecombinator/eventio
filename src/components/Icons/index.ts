2282adc70accc93a943217d976c15e250223c776
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-06 04:23:17 -0300

    feat(components/Icons): Visibility

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,6 +3,7 @@ export {default as Arrow} from './Arrow';
 export {default as Grid} from './Grid';
 export {default as List} from './List';
 export {default as Loading} from './Loading';
+export {default as Visibility} from './Visibility';
 
 // Interfaces
 
@@ -10,4 +11,5 @@ export interface IconProps {
   width?: number;
   height?: number;
   color?: string;
+  onClick?: () => void;
 }
