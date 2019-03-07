31449d14bf45d95d56b6a55e23ba0e00d24eae32
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-07 07:27:20 -0300

    fix(components/Layout/style): reset styles

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -1,6 +1,9 @@
+import reset from 'styled-reset';
+
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  ${reset}
   * { box-sizing: border-box; }
   a { text-decoration: none; }
 
