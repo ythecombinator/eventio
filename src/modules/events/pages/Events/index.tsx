12a523c9d493a3ea51d808252842e4dc436d658e
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-07 01:10:42 -0300

    feat(pages/events): bootstrap page

---
+++ src/modules/events/pages/Events/index.tsx
@@ -0,0 +1,32 @@
+import {Component} from 'react';
+import {compose} from 'redux';
+
+import Head from 'next/head';
+
+import config from 'config/environment';
+
+import connect, {MappedProps} from './Connector';
+
+const appName = config.settings.app.name;
+
+interface Props extends MappedProps {}
+
+class Events extends Component<Props> {
+  componentDidMount() {
+    this.props.list();
+  }
+
+  render() {
+    const {events} = this.props;
+    console.log(this.props);
+    return (
+      <div>
+        <Head>
+          <title>{appName} | Dashboard</title>
+        </Head>
+      </div>
+    );
+  }
+}
+
+export default compose(connect)(Events);
---
+++ src/pages/events.tsx
@@ -0,0 +1,1 @@
+export {default} from 'modules/events/pages/Events';
