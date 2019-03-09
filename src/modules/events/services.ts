7735a7c37faa8505b8ce04231f006a88181c233b
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-09 06:45:32 -0300

    feat(modules/events): services and utils

--- src/modules/events/services.ts
+++ src/modules/events/services.ts
@@ -4,4 +4,12 @@ const list = () => {
   return server.get('events');
 };
 
-export {list};
+const attend = (id: string) => {
+  return server.post(`events/${id}/attendees/me`);
+};
+
+const unattend = (id: string) => {
+  return server.delete(`events/${id}/attendees/me`);
+};
+
+export {list, attend, unattend};
