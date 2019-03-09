f17cdb40e1e653ee5a6c995b5ec6e0341cf283d2
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-08 22:29:04 -0300

    refactor(core/global): remove useless stuff

--- src/core/global/actions.ts
+++
@@ -1,6 +0,0 @@
-import {createAction} from 'redux-act';
-
-export const actions = {
-  showLoading: createAction('global/SHOW_LOADING'),
-  hideLoading: createAction('global/HIDE_LOADING'),
-};
--- src/core/global/sagas.ts
+++
@@ -1,5 +0,0 @@
-export default function*() {
-  if (process.browser) {
-    console.log('Global Sagas');
-  }
-}
