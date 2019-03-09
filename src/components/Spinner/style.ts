e2fec3179f8ca237c7a24d2a344200e861477ac5
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-09 06:54:39 -0300

    fix(components): small UI tweaks

--- src/components/Spinner/style.ts
+++ src/components/Spinner/style.ts
@@ -6,6 +6,7 @@ import styled from 'utils/theme';
 const {rotate} = styles.animation;
 
 const SpinnerView = styled(Loading)<IconProps>`
+  font-size: 63px;
   animation: ${rotate} 2s linear infinite;
 `;
 
