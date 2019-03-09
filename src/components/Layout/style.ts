e115a0c17621f0af61425029eea80ed2852a6aaf
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-09 01:13:40 -0300

    feat(components/Layout): connect to the store

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -1,21 +1,15 @@
-import reset from 'styled-reset';
+import {animations, column, common, content, general, helpers, reset} from 'styles';
 
 import {createGlobalStyle} from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
-  * { box-sizing: border-box; }
-  a { text-decoration: none; }
-
-  html,
-  body {
-    height: 100%;
-    font-family: "Hind", sans-serif;
-    font-size: 16px;
-  }
-  button:focus {
-    outline: 0;
-  }
+  ${animations}
+  ${column}
+  ${common}
+  ${content}
+  ${general}
+  ${helpers}
 `;
 
 export {GlobalStyles};
