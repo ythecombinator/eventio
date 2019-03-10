d24417147242716c2a1bec0b3018d45e482d0c7e
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-03-10 19:29:55 -0300

    fix(utils/url): properly store cookies

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,10 +1,5 @@
-const getDomain = (host: string) => {
-  const regexParse = new RegExp('[a-z-0-9]{2,63}.[a-z.]{2,5}$');
-  return regexParse.exec(host)[0];
-};
-
 const getDomainFromWindow = () => {
-  return getDomain(window.location.hostname);
+  return window.location.hostname;
 };
 
-export {getDomain, getDomainFromWindow};
+export {getDomainFromWindow};
